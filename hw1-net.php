9cbf644dfa9e6c0e9390d29445b0f98d7365b35e
AntonBorovitskiy <bas@raido-global.ru>
2017-05-23 18:21:10 +0300

    hw 1st added 23-05

---
+++ hw1-net.php
@@ -0,0 +1,53 @@
+<!DOCTYPE>
+<html lang="ru">
+    <head>
+        <title>Антон Боровицкий</title>
+        <meta charset="utf-8">
+        <style>
+            body {
+                font-family: sans-serif;  
+            }
+            
+            dl {
+                display: table-row;
+            }
+            
+            dt, dd {
+                display: table-cell;
+                padding: 5px 10px;
+            }
+        </style>
+    </head>
+    <body>
+        <?php
+        $userName = 'Антон Боровицкий';
+        $userAge = '34';
+        $userMail = 'bas@raido-global.ru';
+        $userCity = 'Санкт-Петербург';
+        $userInfo = 'ВЭД/back-end programmer';
+        ?>
+        <h1>Страница пользователя Антон</h1>
+        <dl>
+            <dt>Имя</dt>
+            <dd><?= $userName ?></dd>
+        </dl>
+        <dl>
+            <dt>Возраст</dt>
+            <dd><?= $userAge ?></dd>
+        </dl>
+        <dl>
+            <dt>Адрес электронной почты</dt>
+            <dd>
+                <a href='<?= $userMail ?>'><?= $userMail ?></a>
+            </dd>
+        </dl>
+        <dl>
+            <dt>Город</dt>
+            <dd><?= $userCity ?></dd>
+        </dl>
+        <dl>
+            <dt>О себе</dt>
+            <dd><?= $userInfo ?></dd>
+        </dl>
+    </body>
+</html>
